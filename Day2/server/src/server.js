d323b9d3d2dfc101e07dbce39b70dc89a4598076
Hammad-87 <pakswim@gmail.com>
2020-08-25 15:56:38 +0200

    server.js

---
+++ Day2/server/src/server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const {join} = require("path")
+const listEndPoints = require("express-list-endpoints");
+const mongoose = require("mongoose")
+const usersRouter = require("./services/users")
+
+const server = express()
+server.use(express.json())
+server.use("/users", usersRouter)
+
+const port = 3000
+
+console.log(listEndPoints(server))
+
+mongoose
+.connect("mongodb://localhost:27017/basic-auth",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+.then(
+    server.listen(port, () =>{
+        console.log("Running on port", port)
+    })
+)
+.catch((err) => console.log(err))
